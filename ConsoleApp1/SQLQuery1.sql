ff6239f9ddc9fa655779869085c6ace8ef9626a2
Tertyshni <a19103296@gmail.com>
2026-02-08 22:08:42 +0200

    Add files via upload

---
+++ ConsoleApp1/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Teachers (
+    [Id]                INT IDENTITY(1,1)       NOT NULL PRIMARY KEY,
+    [EmploymentDate]    DATE                    NOT NULL CHECK (EmploymentDate >= '1990-01-01'),
+    [Name]              NVARCHAR(MAX)           NOT NULL CHECK (LEN(Name) > 0),
+    [Premium]           MONEY                   NOT NULL DEFAULT 0 CHECK (Premium >= 0),
+    [Salary]            MONEY                   NOT NULL CHECK (Salary > 0),
+    [Surname]           NVARCHAR(MAX)           NOT NULL CHECK (LEN(Surname) > 0)
+);
---
+++ ConsoleApp1/SQLQuery2.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE Faculties (
+    [Id]        INT IDENTITY(1,1)       NOT NULL PRIMARY KEY,
+    [Name]      NVARCHAR(100)           NOT NULL UNIQUE CHECK (LEN(Name) > 0)
+);
